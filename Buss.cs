3cacd39c8412b0219ae542b72edc5b4f4078b0aa
Libre255 <briangbv@outlook.com>
2021-10-10 19:33:51 +0200

    finished

--- Buss.cs
+++ Buss.cs
@@ -1,52 +1,106 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 namespace Bussen{
   
   public class BussConfig {
-    public int [] passagerare;
-		public int antal_passagerare;
-    public void Run()
-    {
-			Console.WriteLine("Welcome to the awesome Buss-simulator");
-			//Här ska menyn ligga för att göra saker
-			//Jag rekommenderar switch och case här
-			//I filmen nummer 1 för slutprojektet så skapar jag en meny på detta sätt.
-			//Dessutom visar jag hur man anropar metoderna nedan via menyn
-			//Börja nu med att köra koden för att se att det fungerar innan ni sätter igång med menyn.
-			//Bygg sedan steg-för-steg och testkör koden.
-		}
-    //Metoder för betyget E
-		
-		public void add_passenger()
-		{
-			//Lägg till passagerare. Här skriver man då in ålder men eventuell annan information.
-			//Om bussen är full kan inte någon passagerare stiga på
-		}
-    public void print_buss()
-		{
-			//Skriv ut alla värden ur vektorn. Alltså - skriv ut alla passagerare
-		}
-    public int calc_total_age()
-		{
-			//Beräkna den totala åldern. 
-			//För att koden ska fungera att köra så måste denna metod justeras, alternativt att man temporärt sätter metoden med void
-      return  1;
-    }
-    public double calc_average_age()
-		{
-			//Betyg C
-			//Beräkna den genomsnittliga åldern. Kanske kan man tänka sig att denna metod ska returnera något annat värde än heltal?
-			//För att koden ska fungera att köra så måste denna metod justeras, alternativt att man temporärt sätter metoden med void
-       return  1;
-    }
-    public int max_age()
-		{
-			//Betyg C
-			//ta fram den passagerare med högst ålder. Detta ska ske med egen kod och är rätt klurigt.
-			//För att koden ska fungera att köra så måste denna metod justeras, alternativt att man temporärt sätter metoden med void
-      return 1;
-  	}
+    private int [] Passengers = {12,43,23};
+	private string[] MenuOptions = {
+			"Add passangeres",
+			"Print the all the passengers", 
+			"Calculate the total age of all passengers", 
+			"Calculate the average age",
+			"Show the oldest passenger age",
+			"Exit"
+		};
+    public void Run(){
+		Menu myMenu = new Menu(MenuOptions);
 
+     	int OptionIndex = myMenu.Run();
+
+		switch (OptionIndex){
+			case 0:add_passenger();
+				break;
+			case 1:print_buss();
+				break;
+			case 2:calc_total_age();
+				break;
+			case 3:calc_average_age();
+				break;
+			case 4:max_age();
+				break;
+			case 5:Exit();
+				break;
+			default:WriteLine("Welcom Aboard!");		
+			break;
+		}
+	}  
+	
+	private void Exit(){
+		WriteLine("Hope you enjoy your ride");
+	}
+	private void reRunMenu(){
+		WriteLine("<< Press any key to go back to the menu  >>");
+		ReadKey(true);
+		Run();
+	}
+	private void add_passenger(){
+		Clear();
+		WriteLine($"Current Passangers: {Passengers.Length}");
+		
+		if(Passengers.Length == 25){
+			WriteLine("The buss is full!!");
+			
+		}else{
+			WriteLine("What age are you?");
+			var input = ReadLine();
+			int answer;
+            bool parseSuccess = int.TryParse(input, out answer);
+			if(parseSuccess){
+				var tempList = Passengers.ToList();
+				tempList.Add(answer);
+				Passengers = tempList.ToArray();
+			}else{
+				WriteLine("You can only write numbers!");
+			}
+		}
+		reRunMenu();
+	}
+	private void print_buss(){
+		Clear();
+		for (var i = 0; i < Passengers.Length; i++)	{
+			WriteLine($"Passenger {i+1} has the age: {Passengers[i]}");
+		}
+		reRunMenu();
+	}
+	private void calc_total_age(){
+		Clear();
+		int totalAge = 0;
+		
+		for (var i = 0; i < Passengers.Length; i++)	{
+			totalAge += Passengers[i];
+		}
+
+		WriteLine($"The total age of all passengers are: {totalAge}");
+		reRunMenu();
+	}
+	private void calc_average_age(){
+		Clear();
+		double averegeAge = 0;
+		
+		for (var i = 0; i < Passengers.Length; i++)	{
+			averegeAge += Passengers[i];
+		}
+
+		WriteLine($"The average age of the passengers in the buss are: {averegeAge/2}");
+		reRunMenu();
+	}
+	private void max_age(){
+		Clear();
+		WriteLine($"The oldest person in the buss are: {Passengers.Max()}");
+		reRunMenu();
+	}
   }
 }
--- Menu.cs
+++ Menu.cs
@@ -1,32 +1,49 @@
+using System;
 using static System.Console;
 using static System.ConsoleColor;
 
 namespace Bussen{
-  public class Menu
-  {
-    private int SelectedOptionIndex;
-    private string[] Options;
-    private string Prompt;
-    public Menu(string prompt, string[] options){
-      Prompt = prompt;
-      Options = options;
-      SelectedOptionIndex = 1;
+	public class Menu{
+		private int OptionIndex;
+		private string[] Options;
+		public Menu(string[] options){	
+			Options = options;
+			OptionIndex = 1;
 
-    } 
-    public void DisplayOptions (){
-      WriteLine(Prompt);
-      for (var i = 0; i < Options.Length; i++)
-      { 
-        if(i == SelectedOptionIndex){
-          ForegroundColor = Black;
-          BackgroundColor = White;
-        }else{
-          ResetColor();
-        }
-          WriteLine($"<< {Options[i]} >>");
-      }
-      ResetColor();
-    }
-  }
+		} 
+		private void DisplayOptions (){
+			WriteLine("Welcome to the awesome Buss-simulator");
+			WriteLine("Use UP and DOWN arrows to nagivate");
 
+			for (var i = 0; i < Options.Length; i++)
+			{ 
+				if(i == OptionIndex){
+				ForegroundColor = Black;
+				BackgroundColor = White;
+				}else{
+				ResetColor();
+				}
+				WriteLine($"<< {Options[i]} >>");
+			}
+			ResetColor();
+		}
+
+		public int Run(){
+			ConsoleKey keyPressed;
+			do{
+				Clear();
+				DisplayOptions();
+				keyPressed = ReadKey(true).Key;
+
+				if(keyPressed == ConsoleKey.UpArrow && OptionIndex > 0){
+					OptionIndex --;
+				}else if(keyPressed == ConsoleKey.DownArrow && OptionIndex < Options.Length - 1){
+					OptionIndex ++; 
+				}
+				
+			}while(keyPressed != ConsoleKey.Enter);
+			
+			return OptionIndex;
+		}
+	}
 }
